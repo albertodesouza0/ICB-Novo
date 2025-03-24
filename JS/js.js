8e7d25a6ced06ede8e07783f7550532855c9077d
albertodesouza0 <147955665+albertodesouza0@users.noreply.github.com>
2025-03-24 00:18:56 -0300

    Cadastro básico
    Página de cadastro básico adicionada

---
+++ JS/js.js
@@ -0,0 +1,9 @@
+function formatarCPF(campo) {
+    let cpf = campo.value.replace(/\D/g, ""); // Remove tudo que não for número
+
+    if (cpf.length > 3) cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+    if (cpf.length > 6) cpf = cpf.replace(/(\d{3})\.(\d{3})(\d)/, "$1.$2.$3");
+    if (cpf.length > 9) cpf = cpf.replace(/(\d{3})\.(\d{3})\.(\d{3})(\d)/, "$1.$2.$3-$4");
+
+    campo.value = cpf;
+}
